1980466b906b4b4654a1f02e8346f02679cd9f5d
rzientara <rzientara96@gmail.com>
2019-11-04 14:43:44 -0600

    moved routes in main.py to seperate files and imported them to main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from flask import Flask, request, redirect, render_template
+import awards
+import photos
+import info
 app = Flask('app')
 
 @app.route('/')
 def index():
   return render_template('home.html')
-
-@app.route('/info')
-def info():
-  return render_template('info.html')
-
-@app.route('/photos')
-def photos():
-  return render_template('photos.html')
-
-@app.route('/awards')
-def awards():
-  return render_template('awards.html')
 
 app.run(host='0.0.0.0', port=8080)

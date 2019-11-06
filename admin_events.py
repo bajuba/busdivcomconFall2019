43381770bdf567bb07834cba1a0ff24c6be1020e
runderwood <runderwood2016@gmail.com>
2019-11-06 16:16:35 -0600

    admin user athentication complete

--- admin_events.py
+++ admin_events.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from database import app, Events, Admins
 
-@app.route("/events")
+@app.route("/admin_events")
 def admin_events():
     if(session["admin"] == True):
         return render_template("admin_events.html")
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("admin_login"))
--- admin_students.py
+++ admin_students.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from database import app, Students, Admins
 
-@app.route("/students")
+@app.route("/admin_students")
 def admin_students():
     if(session["admin"] == True):
         return render_template("admin_students.html")
     else:
-        return redirect(url_for("login"))
+        return redirect(url_for("admin_login"))
